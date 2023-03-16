82b9b1022ce0a162e05ff00ab2e908c0fc93b786
Connor Berg <bergconnor@gmail.com>
2023-03-15 19:36:49 -0500

    initial commit

---
+++ test/temp.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Temp = require('../lib/temp-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/temp-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Temp.TempStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
